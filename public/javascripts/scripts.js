2f39969ee10cecf64c443065508eff62af435aca
tony <tonyyin89@gmail.com>
2016-09-18 17:27:56 -0700

    set cookies

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   setVotes();
+  checkVoted();
 });
 
 function setVotes() {
   $.get('/votes', (data) => {
     var votesUpgrade = data.votesUpgrade;
     var votesWait = data.votesWait;
-    
+
     var votesUpgradeElem = document.getElementById('votes-upgrade')
     votesUpgradeElem.innerHTML = votesUpgrade;
     var votesWaitElem = document.getElementById('votes-wait')
@@ -14,25 +15,50 @@ function setVotes() {
     
     var verdict = document.getElementById('verdict');
     if (votesUpgrade > votesWait) {
-      verdict.innerHTML = 'A: You should upgrade.'
+      verdict.innerHTML = 'upgrade.'
     } else if (votesUpgrade < votesWait) {
-      verdict.innerHTML = 'A: You should wait.'
+      verdict.innerHTML = 'wait.'
     } else {
-      verdict.innerHTML = 'A: Standby..'
+      verdict.innerHTML = '??'
     };
   });
 };
+
+function checkVoted() {
+  var voted = Cookies.get('iphone7');
+  var yourVote = document.getElementById('your-vote');
+  if (typeof voted !== 'undefined') {
+    var overlays = document.getElementsByClassName('overlay');
+    Array.from(overlays).forEach((e, i, array) => {
+      array[i].style.display = 'none';
+    });
+    yourVote.innerHTML = 'You voted to ' + voted + '.';
+  } else {
+    yourVote.innerHTML = 'You have not yet voted.';
+  };
+};
+function setVoted(gen, vote) {
+  Cookies.set(gen, vote);
+  setVotes();
+  checkVoted();
+};
+
+/* API */
 function voteUpgrade () {
   $.post('/vote/upgrade',
     {
       gen: 'iphone7',
-      vote: 'upgrade'
-    }, setVotes());
+      vote: 'upgrade',
+      timestamp: Date.now()
+    },
+    setVoted('iphone7', 'upgrade'));
 };
 function voteWait () {
   $.post('/vote/wait',
     {
       gen: 'iphone7',
-      vote: 'wait'
-    }, setVotes());
+      vote: 'wait',
+      timestamp: Date.now()
+    },
+    setVoted('iphone7', 'wait'));
 };
